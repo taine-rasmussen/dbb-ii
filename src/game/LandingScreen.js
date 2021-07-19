6550a1c2a8aece64e485e7898eb387eea5504556
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2021-07-20 10:30:09 +1200

    landing screen works!

--- src/game/LandingScreen.js
+++ src/game/LandingScreen.js
@@ -12,12 +12,25 @@ export default class LandingScreen extends Phaser.Scene {
         this.load.scenePlugin('mergedInput', MergedInput);
     }
 
+    create()
+    {
+        const text = this.add.text(240, 300, "Press A/X to Play!", { fontFamily: "Arial Black", fontSize: 82 });
+        text.setStroke('#000000', 4);
+        //  Apply the gradient fill.
+        const gradient = text.context.createLinearGradient(0, 0, 0, text.height);
+        gradient.addColorStop(0, '#111111');
+        gradient.addColorStop(.5, '#ffffff');
+        gradient.addColorStop(.5, '#aaaaaa');
+        gradient.addColorStop(1, '#111111');
+    
+        text.setFill(gradient);
+    }
     update()    
     {
         let playerNumber = 0
         for (let thisPlayer of this.mergedInput.players){
             playerNumber++
-            this.add.text(200 * playerNumber, 100, `Hello player ${playerNumber}`)
+            this.add.text(200 * playerNumber, 100, `Hello player ${playerNumber}`, { fontSize: 20, color: '#00ff00' })
         }
         for (let thisPlayer of this.mergedInput.players) {
             if (thisPlayer.buttons["B0"]){
