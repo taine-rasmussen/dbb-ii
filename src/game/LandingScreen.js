9a42abdcae3da9dc7c0953a2babb6931b44f365b
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2021-07-21 11:25:56 +1200

    stage select

--- src/game/LandingScreen.js
+++ src/game/LandingScreen.js
@@ -6,38 +6,18 @@ export default class LandingScreen extends Phaser.Scene {
     {
         super({ key: 'LandingScreen' });
     }
-    init()
-    {
-        this.sunMap = {
-            mapData: 'sun_map.json', tileData: "gridtiles.png", backgroundData:  "sun_background.png"
-        }
-        this.eyeMap = {
-            mapData: 'eye_background.json', tileData: "gridtiles.png", backgroundData:  "eye_background.png"
-        }
-        this.dragonMap = {
-            mapData: 'dragon_background.json', tileData: 'gridtiles.png', backgroundData: 'dragon_background.png'
-        }
-        this.playerNumber = 0
-    }
-
     preload ()
     {
         this.load.scenePlugin('mergedInput', MergedInput);
         this.load.image('button', './assets/select.png')
         this.load.image('bg', './assets/basic_sky.png')
+        this.load.image('sun', './assets/sun_screenshot.PNG')
+        this.load.image('dragon', './assets/dragon_screenshot.PNG')
+        this.load.image('eye', './assets/eye_screenshot.PNG')
+        
     }
-
     create()
     {
-        // const text = this.add.text(240, 300, "Press A/X to Play!", { fontFamily: "Arial Black", fontSize: 82 });
-        // text.setStroke('#000000', 4);
-        // //  Apply the gradient fill.
-        // const gradient = text.context.createLinearGradient(0, 0, 0, text.height);
-        // gradient.addColorStop(0, '#111111');
-        // gradient.addColorStop(.5, '#ffffff');
-        // gradient.addColorStop(.5, '#aaaaaa');
-        // gradient.addColorStop(1, '#111111');
-        // text.setFill(gradient);
         const bg = this.add.image(0,0,'bg').setScale(2)
         const sunButton = this.add.image(200, 600, 'button').setInteractive().setScale(0.5);
         const dragonButton = this.add.image(600, 600, 'button').setInteractive().setScale(0.5);
@@ -45,6 +25,9 @@ export default class LandingScreen extends Phaser.Scene {
         sunButton.once('pointerup', this.startSunMap, this)
         dragonButton.once('pointerup', this.startDragonMap, this)
         eyeButton.once('pointerup', this.startEyeMap, this)
+        const sunShot = this.add.image(200, 450, 'sun').setScale(0.3)
+        const dragonShot = this.add.image(600, 450, 'dragon').setScale(0.3)
+        const eyeShot = this.add.image(1000, 450, 'eye').setScale(0.3)
     }
     startEyeMap()
     {
@@ -63,7 +46,7 @@ export default class LandingScreen extends Phaser.Scene {
     update()    
     {
         this.mergedInput.players.forEach((player, i) => {
-            this.add.text(200 * (i + 1), 100, `Hello player ${i + 1}`, { fontSize: 20, color: '#00ff00' })
+            this.add.text(200 * i, 100, `Hello player ${i + 1}`, { fontSize: 20, color: '#00ff00' })
         })
     }
 } 
