7b21f32da6de9edc1021e3bd3c79900ff9bf9bb0
Edwyn <81662201+edwyn-mcmillan@users.noreply.github.com>
2021-07-21 17:33:06 +1200

    Revert "Feature/dynamic spawns"

--- src/game/LandingScreen.js
+++ src/game/LandingScreen.js
@@ -33,49 +33,41 @@ export default class LandingScreen extends Phaser.Scene {
   
   create()
   {
-    // const text = this.add.text(240, 300, "Press A/X to Play!", { fontFamily: "Arial Black", fontSize: 82 });
-    // text.setStroke('#000000', 4);
-    // //  Apply the gradient fill.
-    // const gradient = text.context.createLinearGradient(0, 0, 0, text.height);
-    // gradient.addColorStop(0, '#111111');
-    // gradient.addColorStop(.5, '#ffffff');
-    // gradient.addColorStop(.5, '#aaaaaa');
-    // gradient.addColorStop(1, '#111111');
-    // text.setFill(gradient);
-    const bg = this.add.image(0,0,'bg').setScale(2)
-    const sunButton = this.add.image(200, 600, 'button').setInteractive().setScale(0.5);
-    const dragonButton = this.add.image(600, 600, 'button').setInteractive().setScale(0.5);
-    const eyeButton = this.add.image(1000, 600, 'button').setInteractive().setScale(0.5);
-    sunButton.once('pointerup', this.startSunMap, this)
-    dragonButton.once('pointerup', this.startDragonMap, this)
-    eyeButton.once('pointerup', this.startEyeMap, this)
+    let map1Spawns = [[100, 100], [1000, 100], [650, 200], [300, 400], [1000, 400]]
+    const bg = this.add.image(640, 360, 'bg').setScale(0.8)
+    const sunShot = this.add.image(310, 565, 'sun').setInteractive().setScale(0.3)
+    const dragonShot = this.add.image(645, 565, 'dragon').setInteractive().setScale(0.3)
+    const eyeShot = this.add.image(970, 565, 'eye').setInteractive().setScale(0.3)
+    sunShot.once('pointerup', this.startSunMap, this)
+    dragonShot.once('pointerup', this.startDragonMap, this)
+    eyeShot.once('pointerup', this.startEyeMap, this)
+    const pickAMap = this.add.text(390, 400, 'Click a map to play!', {
+        fontFamily: 'Ruslan', 
+        fontSize: 40
+    }).setShadow(2, 2, "#333333", 2, false, true);
   }
   startEyeMap()
   {
     let playerNumber = this.mergedInput.players.length
-    let eyeMap = [[100, 500], [1175, 550], [475, 600], [810, 600], [495, 450], [820, 450], [225, 150], [1070, 150], [480 ,50], [800, 50], [645, 50], [645, 500]] 
-      this.scene.start('Arena', 
-      {
-        mapData: 'eye_background.json',
-        tileData: "gridtiles.png", 
-        backgroundData:  "eye_background.png", 
-        numberOfPlayers: playerNumber,
-        spawns:  eyeMap
-      })
+    this.scene.start('Arena', { mapData: 'eye_background.json',
+     tileData: "gridtiles.png",
+      backgroundData:  "eye_background.png",
+       numberOfPlayers: playerNumber,
+        spawns:  [[280, 600], [1000, 150], [500, 300], [400, 100]]
+    })
   }
   startDragonMap()
   {
     let playerNumber = this.mergedInput.players.length
-    let drangonMap = [[80, 100], [350, 50], [622, 100], [945, 25], [1250, 25], [1185, 400], [945, 550], [722, 325], [530, 375], [200, 550]]
-      this.scene.start('Arena', 
-      { 
-        mapData: 'dragon_background.json', 
-        tileData: "gridtiles.png", 
-        backgroundData:  
-        "dragon_background.png",
-        numberOfPlayers: playerNumber, 
-        spawns:  drangonMap
-      })
+    this.scene.start('Arena', 
+    { 
+      mapData: 'dragon_background.json', 
+      tileData: "gridtiles.png", 
+      backgroundData:  
+      "dragon_background.png",
+      numberOfPlayers: playerNumber, 
+      spawns:  [[280, 600], [1000, 150], [500, 300], [400, 100]]
+    })
   }
   startSunMap()
   {
